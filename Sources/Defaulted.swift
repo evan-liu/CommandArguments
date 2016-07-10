a6aac934d76a960c77c4fe2478ca7960710b9cb5
Evan Liu <evancoding@gmail.com>
2016-07-10 19:46:19 +1200

    Add `Defaulted` arguments

---
+++ Sources/Defaulted.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+public class DefaultedOptionT<T: ArgumentConvertible> {
+    public var value: T
+    
+    var name: OptionName
+    let usage: String?
+    public init(_ defaultValue: T, longName: String? = nil, shortName: String? = nil, usage: String? = nil) {
+        value = defaultValue
+        self.name = (longName, shortName)
+        self.usage = usage
+    }
+}
+
+public final class DefaultedOption: DefaultedOptionT<String> {
+    public override init(_ defaultValue: String, longName: String? = nil, shortName: String? = nil, usage: String? = nil) {
+        super.init(defaultValue, longName: longName, shortName: shortName, usage: usage)
+    }
+}
+
+public class DefaultedOperandT<T: ArgumentConvertible> {
+    public var value: T
+    
+    var name: String?
+    let usage: String?
+    public init(_ defaultValue: T, name: String? = nil, usage: String? = nil) {
+        value = defaultValue
+        self.name = name
+        self.usage = usage
+    }
+    
+}
+
+public final class DefaultedOperand: DefaultedOperandT<String> {
+    public override init(_ defaultValue: String, name: String? = nil, usage: String? = nil) {
+        super.init(defaultValue, name: name, usage: usage)
+    }
+}
+
+// ----------------------------------------
+// MARK: Internal
+// ----------------------------------------
+protocol DefaultedArgumentProtocol: Parsable {
+    associatedtype Value: ArgumentConvertible
+    var value: Value { get set }
+}
+
+class DefaultedArgumentParser<Target: DefaultedArgumentProtocol>: Parser {
+    let target: Target
+    init(target: Target) {
+        self.target = target
+    }
+    
+    var canTakeValue = true
+    func parseValue(_ value: String) {
+        if let value = Target.Value.parseArgument(value) {
+            target.value = value
+        }
+        canTakeValue = false
+    }
+}
+
+extension DefaultedArgumentProtocol {
+    var parser: Parser {
+        return DefaultedArgumentParser(target: self)
+    }
+}
+
+extension DefaultedOptionT: OptionProtocol, DefaultedArgumentProtocol { }
+extension DefaultedOperandT: OperandProtocol, DefaultedArgumentProtocol { }
--- Tests/CommandArguments/DefaultedTests.swift
+++ Tests/CommandArguments/DefaultedTests.swift
@@ -1,40 +1,40 @@
 import XCTest
 import CommandArguments
 
-class OptionalTests: XCTestCase {
+class DefaultedTests: XCTestCase {
 
-    func testOptionalOperand() {
+    func testDefaultedOperand() {
         struct TestArgs: CommandArguments {
             var a = Operand()
-            var b = OptionalOperand()
+            var b = DefaultedOperand("x")
         }
         
         var args1 = TestArgs()
         try! args1.parse(["1"])
-        XCTAssertNil(args1.b.value)
+        XCTAssertEqual(args1.b.value, "x")
         
         var args2 = TestArgs()
         try! args2.parse(["1", "2"])
         XCTAssertEqual(args2.b.value, "2")
     }
     
-    func testOptionalStringOption() {
+    func testDefaultedStringOption() {
         struct TestArgs: CommandArguments {
             var a = Option()
-            var b = OptionalOption()
+            var b = DefaultedOption("y")
         }
         
         var args1 = TestArgs()
         try! args1.parse(["-a=x"])
-        XCTAssertNil(args1.b.value)
+        XCTAssertEqual(args1.b.value, "y")
         
         var args2 = TestArgs()
         try! args2.parse(["-a=x", "-b"])
-        XCTAssertNil(args2.b.value)
+        XCTAssertEqual(args1.b.value, "y")
         
         var args3 = TestArgs()
         try! args3.parse(["-a=x", "-b="])
-        XCTAssertNil(args3.b.value)
+        XCTAssertEqual(args1.b.value, "y")
         
         var args4 = TestArgs()
         try! args4.parse(["-a=x", "-b=4"])
@@ -45,4 +45,5 @@ class OptionalTests: XCTestCase {
         XCTAssertEqual(args5.b.value, "5")
     }
 
+    
 }
